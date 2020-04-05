5bd9b78fafa4a43a33735bcf3258efe96d67aff7
ddd <ddddd@naver.com>
2020-04-05 18:11:09 +0900

    .

--- st01.Python기초/py08반복문/py08_32_무한구구단.py
+++ st01.Python기초/py08반복문/py08_32_무한구구단.py
@@ -3,6 +3,20 @@
 while True:
 
     # py08_26 코드 복사
+    if 시작단수 > 종료단수:
+    임시값 = 시작단수
+    시작단수 = 종료단수
+    종료단수 = 임시값
+
+    else:
+        pass
+
+    for i in range(시작단수, 종료단수, 1):
+        for j in range(1, 10, 1):
+            str1 = "%s * %s = %s" % (i, j, i*j)
+            print(str1, end=",")
+         print()
+
     int1 = int(input("정수를 입력하세요"))
 
     if int1 >= 0:
